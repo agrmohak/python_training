e70d1fa2ab7ff03970e6328ce6c0e9f604d3ee9a
Agrawal <mohak.agrawal@ecolab.com>
2023-10-04 23:14:11 +0530

    Updated linked list

--- assignments/doubly_linked_list.py
+++ assignments/doubly_linked_list.py
@@ -1,89 +1,93 @@
 class Node:
-    def __init__(self, value,next=None, previous=None):
-        self._value=value
+    def __init__(self, data,next=None, previous=None):
+        self._data=data
         self._next=next
         self._previous=previous
     def __str__(self):
-        return f'{self._value}'
+        return f'{self._data}'
+    def __repr__(self) -> str:
+        return self.__str__()
         
 class LinkedList:
     def __init__(self):
         self._first=None
         self._tail = None
+        self.__len = 0
+    class Iterator:
+        def __init__(self, list):
+            self._list=list
+            self._current=None
+            
+        def __next__(self):
+            if not self._current:
+                self._current=self._list._first
+            else:
+                self._current=self._current._next
+                
+            if self._current:
+                return self._current._data
+            else:
+                raise StopIteration()
+    def __iter__(self):
+        return LinkedList.Iterator(self)
 
-    def append(self, value):
+    def append(self, *args):
+        for x in args:
+            self._append(x)
+        
+    def _append(self, data):
         if self._first==None: # list is empty
-            self._first=Node(value)
+            self._first=Node(data)
             self._tail = self._first
         else: # add to the end of a non-empty list
-            new_node = Node(value, previous=self._tail)
+            new_node = Node(data, previous=self._tail)
             self._tail._next = new_node
             self._tail = new_node
-
-    def info(self):
-        if self._first==None: 
-            return "LinkedList(empty)"
-        str="LinkedList(\t"
-        n=self._first
-        while n:
-            str+=f'{n._value}\t'
-            n=n._next
-        str+=")"
-        return str
+        self.__len += 1
 
     def __len__(self):
-        c=0
-        n=self._first
-        while n:
-            c+=1
-            n=n._next
-        return c
-    def get_node(list,index):
-        n=list._first
-        for i in range(index):
-            n=n._next
-            if n==None:
-                return None
+        return self.__len
+    def __get_node(self,index):
+        for i, data in enumerate(self):
+            if i == index:
+                return data
         else:
-            return n
-        
-    def get(list,index):
-       return list.get_node(index)._value
+            raise IndexError('list index out of range')
 
-    def set(list,index,value):
-        n = list.get_node(index)
+    def set(self,index,data):
+        n = self.__get_node(index)
         if not n:
             return
-        n._value=value
+        n._data=data
 
-    def insert(list, index, value):
-        if len(list)==index:
-            new_node = Node(value, previous=list._tail)
-            list._tail._next = new_node
-            list._tail = new_node
+    def insert(self, index, data):
+        if len(self)==index:
+            new_node = Node(data, previous=self._tail)
+            self._tail._next = new_node
+            self._tail = new_node
             return
         
-        y = list.get_node(index)
+        y = self.__get_node(index)
         
         if not y:
             return
 
         x=y._previous
 
-        new_node=Node(value,previous=x,next=y)
+        new_node=Node(data,previous=x,next=y)
         
         if x:
             x._next=new_node
         else:
-            list._first=new_node
+            self._first=new_node
 
         if y._next == None:
-            list._tail = new_node
+            self._tail = new_node
 
         y._previous=new_node
 
-    def remove(list, index):
-        n = list.get_node(index)
+    def remove(self, index):
+        n = self.__get_node(index)
         if not n:
             return 
         
@@ -93,13 +97,13 @@ class LinkedList:
         if x:
             x._next=y
         else:
-            list._first=y
+            self._first=y
 
         if y:
             y._previous=x
         else:
-            list._tail = x
-        return n._value
+            self._tail = x
+        return n._data
     
     def clear(self):
         temp = self._first
@@ -107,36 +111,33 @@ class LinkedList:
             self.remove(0)
             temp = temp._next
     def count(self, data):
-        temp = self._first
         count = 0
-        while temp != None:
-            if(temp._value == data):
-                count += 1
-            temp = temp._value
+        for x in self:
+            if x == data:
+                count +=1
         return count
     
     def __str__(self):
-        output = '('
-        temp = self._first
-        while temp != None:
-            output += str(temp._value) + ','
-            temp = temp._next
-        output +=')'
+        output = '['
+        for x in self:
+            output += str(x)
+            if(x!=None):
+                output += ', '
+        output +=']'
         return output
-
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
     def __contains__(self, data):
-        temp = self._first
-        while temp != None:
-            if temp._value == data:
+
+        for x in self:
+            if x == data:
                 return True
-            temp = temp._next
         return False
 
-    def __getitem__(self,index):
-        n=self._first
-        for i in range(index):
-            n=n._next
-            if n==None:
-                return None
-        else:
-            return n
+    def __getitem__(self,indexer):
+        pos = indexer
+        if type(pos) == int and indexer < 0:
+            pos = self.__len + indexer
+        return self.__get_node(pos)
